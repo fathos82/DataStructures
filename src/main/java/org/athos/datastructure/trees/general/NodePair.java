5b972b031072e76f1b49a6e0c95fc6e9f420d193
fathos82 <72706252+fathos82@users.noreply.github.com>
2023-11-28 08:33:07 -0300

    Delete src/main/java/org/athos/datastructure/trees directory

--- src/main/java/org/athos/datastructure/trees/general/NodePair.java
+++
@@ -1,8 +0,0 @@
-package org.athos.datastructure.trees.general;
-
-
-import org.athos.datastructure.trees.GenericNode;
-
-public record NodePair<T extends Comparable<T>, Node extends GenericNode<T, Node>>(Node parent, Node child) {
-
-}
